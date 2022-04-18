00a11d8bbe24db7f58431761f37b523066c21905
Badrutdin <ataev.b@introvert.bz>
2022-04-19 02:10:09 +0300

    commit

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,4 +1,4 @@
-const filter = function ($filter) {
+function filter($filter) {
   const parent = $filter.get(0);
   const title = parent.querySelector('.filter__title');
   const icon = parent.querySelector('.filter__icon');
@@ -25,3 +25,43 @@ $(document).ready(function () {
     filter($(this))
   })
 })
+
+function ajaxRender({$parent, targetAttribute, containerClass, activeClass, url, data}) {
+  const storage = {}
+  const $items = $parent.find('[' + targetAttribute + ']')
+  const $textContainer = $parent.find(containerClass)
+  const active = activeClass
+  $items.on('click', function () {
+    const $item = $(this)
+    let text;
+    let key = $item.attr(targetAttribute);
+    $.each($items, function (k, v) {
+      $(v).removeClass(active,200)
+    })
+    $item.addClass(active,200)
+    if (!storage[key]) {
+      $.ajax({
+        url: url,
+        data: data,
+        success: function (response) {
+          text = response[key]['text'];
+          storage[key] = text
+          $textContainer.text(storage[key])
+        }
+      })
+    } else {
+      $textContainer.text(storage[key])
+    }
+  })
+  $parent.find('.' + active).trigger('click')
+}
+
+ajaxRender({
+  $parent: $('.c-product-review'),
+  targetAttribute: 'data-text',
+  containerClass: '.c-product-review__text',
+  activeClass: 'c-product-review__item_active',
+  url: 'ajax/product-review.json',
+  data: {articul: 1234456734}
+})
+
