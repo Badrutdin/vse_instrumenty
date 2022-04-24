3314704ef0699889afe98c43543763c83933e075
Badrutdin <86923895+Badrutdin@users.noreply.github.com>
2022-04-25 00:50:24 +0300

    commit

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,3 +1,14 @@
+if (!Object.entries) {
+  Object.entries = function (obj) {
+    var ownProps = Object.keys(obj),
+      i = ownProps.length,
+      resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
+}
 function dropdown($dropdown) {
   const parent = $dropdown.get(0);
   const title = parent.querySelector('.c-dropdown__title');
